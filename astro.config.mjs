c9ce3e70c09f589715d02c413a87d1e9df48ba2b
wayne soares <weinnysoares.wad@gmail.com>
2024-05-25 22:36:55 -0300

    feat: add configs do github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const host = "https://mobilizacaoconservadora.github.io"
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: host
 });
