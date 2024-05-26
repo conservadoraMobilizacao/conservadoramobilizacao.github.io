cf0a576c6b74537422282f65665d799d678c7990
wayne soares <weinnysoares.wad@gmail.com>
2024-05-25 22:43:52 -0300

    fix: remoção de host do arquivo astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-const host = "https://mobilizacaoconservadora.github.io"
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: host
+  integrations: [tailwind()]
 });
